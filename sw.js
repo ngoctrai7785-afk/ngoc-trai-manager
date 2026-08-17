7b66b856d9b811f19b758df4a0b6120227f6416e
ngoctrai7785-afk <ngoctrai7785@gmail.com>
2026-08-17 12:14:12 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ngoc-trai-manager-v53';
+const CACHE='ngoc-trai-manager-v54';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./QR_NGOC_TRAI_MANAGER.png'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));
@@ -11,13 +11,11 @@ self.addEventListener('activate',event=>{
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET')return;
   if(event.request.mode==='navigate'){
-    event.respondWith(
-      fetch(event.request,{cache:'no-store'}).then(response=>{
-        const copy=response.clone();
-        caches.open(CACHE).then(cache=>cache.put('./index.html',copy));
-        return response;
-      }).catch(()=>caches.match('./index.html'))
-    );
+    event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put('./index.html',copy));
+      return response;
+    }).catch(()=>caches.match('./index.html')));
     return;
   }
   event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));
